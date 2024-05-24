0e3c520260ff2942801ba70aa723cb0b04ce89d6
RetroHoboSpot <160140131+RetroHoboSpot@users.noreply.github.com>
2024-05-24 08:04:08 +0800

    Update Choco.ps1

--- Choco.ps1
+++ Choco.ps1
@@ -1,7 +1,7 @@
 # Install Chocolatey
 #installs choco and the below apps 
 #spot 
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+#Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install packages using Chocolatey
 $packages = @(
