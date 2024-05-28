5f61262d0852437cc2be48990a100bfe9552a169
RetroHoboSpot <160140131+RetroHoboSpot@users.noreply.github.com>
2024-05-28 15:09:13 +0800

    Update Choco.ps1

--- Choco.ps1
+++ Choco.ps1
@@ -1,8 +1,16 @@
-# Install Chocolatey
+# Function to check if the script is running as admin and relaunch if not
+function Ensure-RunAsAdmin {
+    if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+        Write-Output "Restarting script with administrative privileges..."
+        Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+        Exit
 #installs choco and the below apps 
-#spot 
-#Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+Set-ExecutionPolicy Bypass -Scope Process -Force;
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
+    Write-Output "Chocolatey installed successfully."
+    
 # Install packages using Chocolatey
 $packages = @(
     "VLC",
